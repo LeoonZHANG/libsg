269019ed2afa6dc0a947878dec92b18418b828b9
Canmor Lam <canmor.lam@gmail.com>
2016-08-20 13:03:18 +0800

    Implement uuid by guid on Windows

--- src/math/uuid.c
+++ src/math/uuid.c
@@ -6,7 +6,33 @@
 
 #include <stdio.h>
 #include <string.h>
+#if defined(WIN32)
+#include <Objbase.h>
+typedef GUID uuid_t;
+
+static void uuid_generate_win32(uuid_t* in)
+{
+	CoCreateGuid(&in);
+}
+
+static void uuid_unparse_lower_win32(uuid_t* id, char* out)
+{
+    int i, j;
+    wchar_t ostr[32+6+3];
+    StringFromGUID2(id, (LPOLESTR)ostr, _countof(ostr));
+    for (i = 1, j = 0; i < 38 + 1; i++, j++)
+        out[j] = tolower(ostr[i]);
+}
+
+# define uuid_generate(id)			uuid_generate_win32(&id)
+# define uuid_generate_random       uuid_generate
+# define uuid_generate_time         uuid_generate
+# define uuid_generate_time_safe    uuid_generate
+# define uuid_unparse_lower(id, out)	uuid_unparse_lower_win32(&id, out)
+
+#else
 #include <uuid/uuid.h>
+#endif
 /* FIXME: There's no uuid_generate_time_safe on macOS, simply use regular one
    instead of *_safe. */
 #if defined(__MACH__)
