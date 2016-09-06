3e2a2c185f70ced5c2ee89cdca287ab031fa9c3a
metalwood <377307289@qq.com>
2016-09-06 16:26:39 +0800

    fix printf msg

--- test/unit/rtsp-kcp/watch.c
+++ test/unit/rtsp-kcp/watch.c
@@ -22,7 +22,7 @@ static void etp_on_open(sg_etp_t *client)
 static void etp_on_message(sg_etp_t *client, char *data, size_t size)
 {
 	int ret = sg_player_put_buf(player, data, size);
-	if (ret != size)
+	if (ret != 0)
 		printf("player put buffer error\n");
 
     printf("recv %lu data\n", size);
