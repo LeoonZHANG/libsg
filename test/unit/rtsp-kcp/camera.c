b30db5499b1b61fc1f499eb32cad273cde5b8ae5
metalwood <377307289@qq.com>
2016-09-06 16:12:00 +0800

    modify udp server

--- test/unit/rtsp-kcp/camera.c
+++ test/unit/rtsp-kcp/camera.c
@@ -228,8 +228,10 @@ static void *udp_thread(void *p)
         else if(_size > 0)
         {
             printf("recv %d data\n", _size);
-            if (etp_c)
+            if (etp_c) {
                 sg_etp_server_send(etp_c, buf, _size);
+                printf("send client %d data\n", _size);
+            }
         }
         usleep(10);
     }
