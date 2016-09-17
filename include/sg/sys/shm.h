48fe606d97853f003d92671098928977192ebd69
metalwood <377307289@qq.com>
2016-09-17 15:49:35 +0800

    add shm header file

---
+++ include/sg/sys/shm.h
@@ -0,0 +1,19 @@
+/*
+ * shm.h
+ * Author: wangwei.
+ * Shared memory for multiple platforms.
+ */
+
+#ifndef LIBSG_SHM_H
+#define LIBSG_SHM_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* LIBSG_SHM_H */
