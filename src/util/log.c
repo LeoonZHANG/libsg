934eee5d98a9c882b386300864a7246a7cffe399
metalwood <377307289@qq.com>
2016-09-23 13:04:21 +0800

    refactor log

--- src/util/log.c
+++ src/util/log.c
@@ -7,30 +7,25 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <string.h>
-#include "../../include/sg/util/log.h"
-#include "../../include/sg/sys/time.h"
-#include "../../include/sg/sys/shell.h"
-#include "../../include/sg/str/vlstr.h"
-#include "../../include/sg/sys/module.h"
+#include <sg/sg.h>
+#include <sg/sys/clock.h>
+#include <sg/sys/shell.h>
+#include <sg/str/vsstr.h>
+#include <sg/sys/module.h>
 
-static int log_colorful = 1;
+static bool log_colorful = true;
 static FILE *fp_log;
 static char fp_log_name[1024] = {0};
 
-void sg_log_set_colorful(void)
+void sg_log_set_colorful(bool on)
 {
-    log_colorful = 1;
-}
-
-void sg_log_cancel_colorful(void)
-{
-    log_colorful = 0;
+    log_colorful = on;
 }
 
 void sg_logging(const char *file, int line, const char *func, enum sg_log_level lv, const char *fmt, ...)
 {
-    sg_vlstr_t *log;
-    sg_vlstr_t *log_fmt;
+    sg_vsstr_t *log;
+    sg_vsstr_t *log_fmt;
     va_list ap;
     char mod_path[1024] = {0};
     char log_path[1024] = {0};
@@ -45,19 +40,19 @@ void sg_logging(const char *file, int line, const char *func, enum sg_log_level 
     va_start(ap, fmt);
 
     if (lv == SGLOGLEVEL_CRIT)
-        log_fmt = sg_vlstrfmt("file:%s, line:%d, function:%s\r\n%s", file, line, func, fmt);
+        log_fmt = sg_vsstr_fmt("file:%s, line:%d, function:%s\r\n%s", file, line, func, fmt);
     else
-        log_fmt = sg_vlstrfmt("%s\r\n", fmt);
+        log_fmt = sg_vsstr_fmt("%s\r\n", fmt);
 
-    log = sg_vlstrfmt_ap(sg_vlstrraw(log_fmt), ap);
+    log = sg_vsstr_fmt_ap(sg_vsstr_raw(log_fmt), ap);
 
-    if (sg_vlstrlen(log) == 0)
+    if (sg_vsstr_len(log) == 0)
         goto end;
 
-    printf("%s", sg_vlstrraw(log));
+    printf("%s", sg_vsstr_raw(log));
     
     sg_module_path(mod_path, 1024);
-    sg_curr_date_time("%Y-%m-%d", curr_date, 100);
+    sg_clock_curr_date_time("%Y-%m-%d", curr_date, 100);
     snprintf(log_path, 1024, "%s.%s.log", mod_path, curr_date);
     if (strcmp(log_path, fp_log_name) != 0 && fp_log) {
         fclose(fp_log);
@@ -68,24 +63,24 @@ void sg_logging(const char *file, int line, const char *func, enum sg_log_level 
     if (!fp_log)
         fp_log = fopen(log_path, "ab+");
     if (fp_log) {
-        sg_curr_date_time("%Y-%m-%d %H:%M:%S ", curr_time, 100);
+        sg_clock_curr_date_time("%Y-%m-%d %H:%M:%S ", curr_time, 100);
         fwrite(curr_time, 1, strlen(curr_time), fp_log);
-        fwrite(sg_vlstrraw(log), 1, sg_vlstrlen(log), fp_log);
+        fwrite(sg_vsstr_raw(log), 1, sg_vsstr_len(log), fp_log);
         fflush(fp_log);
     }
 
 end:
     va_end(ap);
-    sg_vlstrfree(&log);
-    sg_vlstrfree(&log_fmt);
+    sg_vsstr_free(&log);
+    sg_vsstr_free(&log_fmt);
 }
 
 /* have some bugs */
 /*
 void sg_logging(const char *file, int line, const char *func, enum sg_log_level lv, const char *fmt, ...)
 {
-    sg_vlstr_t *log;
-    sg_vlstr_t *log_fmt;
+    sg_vsstr_t *log;
+    sg_vsstr_t *log_fmt;
     va_list ap;
     char mod_path[1024] = {0};
     char log_path[1024] = {0};
@@ -99,7 +94,7 @@ void sg_logging(const char *file, int line, const char *func, enum sg_log_level 
 
     va_start(ap, fmt);
     // Memory leaks.
-    //log = sg_vlstralloc3(strlen(fmt) * 2 + 1);
+    //log = sg_vsstr_alloc3(strlen(fmt) * 2 + 1);
     //if (!log) {
     //    printf("Logging open memory error.\n");
     //    goto end;
@@ -109,41 +104,41 @@ void sg_logging(const char *file, int line, const char *func, enum sg_log_level 
     //printf("log param after fmt:%s", va_arg(ap, char *));
 
     if (lv == SGLOGLEVEL_CRIT)
-        log_fmt = sg_vlstrfmt("file:%s, line:%d, function:%s\r\n%s", file, line, func, fmt);
+        log_fmt = sg_vsstrfmt("file:%s, line:%d, function:%s\r\n%s", file, line, func, fmt);
     else
-        log_fmt = sg_vlstrfmt("%s\r\n", fmt);
+        log_fmt = sg_vsstrfmt("%s\r\n", fmt);
 
-    log = sg_vlstrfmt_ap(sg_vlstrraw(log_fmt), ap);
-    //printf("log fmt:%s", sg_vlstrraw(log_fmt));
-    //printf("log:%s", sg_vlstrraw(log));
+    log = sg_vsstr_fmt_ap(sg_vsstrraw(log_fmt), ap);
+    //printf("log fmt:%s", sg_vsstrraw(log_fmt));
+    //printf("log:%s", sg_vsstrraw(log));
 
-    if (sg_vlstrlen(log) == 0)
+    if (sg_vsstrlen(log) == 0)
         goto end;
 
-    if (log_colorful == 1) {
+    if (log_colorful == true) {
         switch (lv) {
         case SGLOGLEVEL_DBG:
-            sg_shell_colorful_print(sg_vlstrraw(log), SGSHELLCOLOR_BLUE);
+            sg_shell_colorful_print(sg_vsstrraw(log), SGSHELLCOLOR_BLUE);
             break;
         case SGLOGLEVEL_INF:
-            sg_shell_colorful_print(sg_vlstrraw(log), SGSHELLCOLOR_WHITE); // bug here!
+            sg_shell_colorful_print(sg_vsstrraw(log), SGSHELLCOLOR_WHITE); // bug here!
             break;
         case SGLOGLEVEL_WARN:
-            sg_shell_colorful_print(sg_vlstrraw(log), SGSHELLCOLOR_YELLOW);
+            sg_shell_colorful_print(sg_vsstrraw(log), SGSHELLCOLOR_YELLOW);
             break;
         case SGLOGLEVEL_ERR:
-            sg_shell_colorful_print(sg_vlstrraw(log), SGSHELLCOLOR_RED);
+            sg_shell_colorful_print(sg_vsstrraw(log), SGSHELLCOLOR_RED);
             break;
         case SGLOGLEVEL_CRIT:
-            sg_shell_colorful_print(sg_vlstrraw(log), SGSHELLCOLOR_WHITE_ON_RED);
+            sg_shell_colorful_print(sg_vsstrraw(log), SGSHELLCOLOR_WHITE_ON_RED);
             printf("\n");
             break;
         case SGLOGLEVEL_ALERT:
-            sg_shell_colorful_print(sg_vlstrraw(log), SGSHELLCOLOR_WHITE_ON_RED);
+            sg_shell_colorful_print(sg_vsstr_raw(log), SGSHELLCOLOR_WHITE_ON_RED);
             printf("\n");
             break;
         case SGLOGLEVEL_EMERG:
-            sg_shell_colorful_print(sg_vlstrraw(log), SGSHELLCOLOR_WHITE_ON_RED);
+            sg_shell_colorful_print(sg_vsstrraw(log), SGSHELLCOLOR_WHITE_ON_RED);
             printf("\n");
             break;
         default:
@@ -151,7 +146,7 @@ void sg_logging(const char *file, int line, const char *func, enum sg_log_level 
             break;
         }
     } else
-        printf("%s", sg_vlstrraw(log));
+        printf("%s", sg_vsstrraw(log));
     
     sg_module_path(mod_path, 1024);
     sg_curr_date_time("%Y-%m-%d", curr_date, 100);
@@ -167,18 +162,18 @@ void sg_logging(const char *file, int line, const char *func, enum sg_log_level 
     if (fp_log) {
         sg_curr_date_time("%Y-%m-%d %H:%M:%S ", curr_time, 100);
         fwrite(curr_time, 1, strlen(curr_time), fp_log);
-        fwrite(sg_vlstrraw(log), 1, sg_vlstrlen(log), fp_log);
+        fwrite(sg_vsstrraw(log), 1, sg_vsstr_len(log), fp_log);
         fflush(fp_log);
     }
 
 end:
     va_end(ap);
-    sg_vlstrfree(&log);
-    sg_vlstrfree(&log_fmt);
+    sg_vsstrfree(&log);
+    sg_vsstr_free(&log_fmt);
 }
 */
 
 void sg_printf_time_stamp(const char *tag)
 {
-    sg_log_inf("%s:%lu", tag, sg_boot_time_ms());
+    sg_log_inf("%s:%lu", tag, sg_clock_boot_time_ms());
 }
