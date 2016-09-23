a29e0a70c68481605e1c384d10ab899ae9174c5f
metalwood <377307289@qq.com>
2016-09-23 13:06:31 +0800

    rename structure

--- src/sys/flag.c
+++ src/sys/flag.c
@@ -5,13 +5,13 @@
  */
 
 #include <stdlib.h>
+#include <sg/sg.h>
 #include <sg/sys/flag.h>
 #include <sg/sys/mutex.h>
-#include <sg/util/assert.h>
 
 #define FLAG_INIT_VALUE 0x0
 
-struct flag_body {
+struct sg_flag_real {
     int             value;
     sg_mutex_t      *mutex;
 };
@@ -19,16 +19,16 @@ struct flag_body {
 
 sg_flag_t *sg_flag_create(void)
 {
-    struct flag_body *f;
+    struct sg_flag_real *f;
 
-    f = (struct flag_body *)malloc(sizeof(struct flag_body));
+    f = (struct sg_flag_real *)malloc(sizeof(struct sg_flag_real));
     if (!f)
         return NULL;
 
     f->value = FLAG_INIT_VALUE;
     f->mutex = sg_mutex_create();
 
-    return f;
+    return (sg_flag_t *)f;
 }
 
 int sg_flag_read(sg_flag_t *f)
@@ -37,9 +37,9 @@ int sg_flag_read(sg_flag_t *f)
 
     assert(f);
 
-    sg_mutex_lock(f->mutex);
-    value = f->value;
-    sg_mutex_unlock(f->mutex);
+    sg_mutex_lock(((struct sg_flag_real *)f)->mutex);
+    value = ((struct sg_flag_real *)f)->value;
+    sg_mutex_unlock(((struct sg_flag_real *)f)->mutex);
 
     return value;
 }
@@ -48,15 +48,15 @@ void sg_flag_write(sg_flag_t *f, int value)
 {
     assert(f);
 
-    sg_mutex_lock(f->mutex);
-    f->value = value;
-    sg_mutex_unlock(f->mutex);
+    sg_mutex_lock(((struct sg_flag_real *)f)->mutex);
+    ((struct sg_flag_real *)f)->value = value;
+    sg_mutex_unlock(((struct sg_flag_real *)f)->mutex);
 }
 
 void sg_flag_destroy(sg_flag_t *f)
 {
     assert(f);
 
-    sg_mutex_destroy(f->mutex);
+    sg_mutex_destroy(((struct sg_flag_real *)f)->mutex);
     free(f);
 }
