561116abaf8afdaa2e5deb1038371826d02b8034
metalwood <377307289@qq.com>
2016-09-23 13:17:26 +0800

    rename cl to compiler

--- include/sg/util/compiler.h
+++ include/sg/util/compiler.h
@@ -1,29 +1,31 @@
 /**
- * cl.h
+ * compiler.h
  * Author: wangwei.
  * Compiler macros.
  */
 
-#ifndef LIBSG_CL_H
-#define LIBSG_CL_H
+#ifndef LIBSG_COMPILER_H
+#define LIBSG_COMPILER_H
 
 #ifdef __cplusplus
 extern "C" {
 #endif /* __cplusplus */
 
 #if defined(__GNUC__)
-# define SG_CL_GCC
+# define SG_COMPILER_GCC
 #elif defined(_MSC_VER)
-# define SG_CL_MSC
+# define SG_COMPILER_MSVC
+#elif defined(__clang__)
+# define SG_COMPILER_CLANG
 #else
 # error Unsupported compiler.
 #endif
 
 /* Compiling date time. */
-#define SG_CL_DATETIME_STRING (__DATE__ ## __TIME__)
+#define SG_COMPILER_DATETIME_STRING (__DATE__ ## __TIME__)
 
 #ifdef __cplusplus
 }
 #endif /* __cplusplus */
 
-#endif /* LIBSG_CL_H */
+#endif /* LIBSG_COMPILER_H */
