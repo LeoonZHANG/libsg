54b5e70475116b20300501ae69b4565351030094
metalwood <377307289@qq.com>
2016-09-23 13:08:09 +0800

    rename vsstr

--- src/str/base64.c
+++ src/str/base64.c
@@ -5,15 +5,10 @@
  */
 
 #include <string.h>
-#if 0
-# include "../3rdparty/libb64-1.2.1/include/b64/cdecode.h"
-# include "../3rdparty/libb64-1.2.1/include/b64/cencode.h"
-#endif
+#include <sg/sg.h>
 #include "../../3rdparty/b64.c/b64.h"
-#include <sg/str/vlstr.h>
+#include <sg/str/vsstr.h>
 #include <sg/str/base64.h>
-#include <sg/util/assert.h>
-#include <sg/util/log.h>
 
 size_t _base64_calc_enc_len(size_t data_len);
 
@@ -42,12 +37,12 @@ size_t _base64_estimate_dec_len(size_t base64_str_len)
 }
 
 #if 0
-sg_vlstr *sg_base64_easy_enc_buf(void *data, size_t size)
+sg_vsstr *sg_base64_easy_enc_buf(void *data, size_t size)
 {
     int cnt = 0; /* the number of bytes encoded by a single call */
     base64_encodestate s; /* encoder state */
     size_t b64_size;
-    sg_vlstr *b64_str;
+    sg_vsstr *b64_str;
     char *b64_buf;
 
     sg_assert(data);
@@ -67,17 +62,17 @@ sg_vlstr *sg_base64_easy_enc_buf(void *data, size_t size)
     sg_log_inf("cnt %d", cnt);
     b64_buf[cnt - 1] = 0; /* terminator, it's different from demo! */
 
-    b64_str = sg_vlstrndup(b64_buf, b64_size - 1);
-    sg_log_inf("b64_str len %lu.", (size_t)sg_vlstrlen(b64_str));
+    b64_str = sg_vsstrndup(b64_buf, b64_size - 1);
+    sg_log_inf("b64_str len %lu.", (size_t)sg_vsstrlen(b64_str));
     free(b64_buf);
     return b64_str;
 }
 
-struct sg_vlbuf *sg_base64_easy_dec_buf(const char *base64_str)
+struct sg_vsbuf *sg_base64_easy_dec_buf(const char *base64_str)
 {
     int cnt = 0; /* the number of bytes decoded by a single call */
     base64_decodestate s; /* decoder state */
-    struct sg_vlbuf *buf;
+    struct sg_vsbuf *buf;
     void *dec_buf;
     size_t dec_size;
 
@@ -87,7 +82,7 @@ struct sg_vlbuf *sg_base64_easy_dec_buf(const char *base64_str)
     dec_buf = malloc(sg_base64_estimate_dec_len(strlen(base64_str)));
     if (!dec_buf)
         return NULL;
-    buf = sg_vlbuf_create();
+    buf = sg_vsbuf_create();
     if (!buf) {
         free(dec_buf);
         return NULL;
@@ -96,13 +91,13 @@ struct sg_vlbuf *sg_base64_easy_dec_buf(const char *base64_str)
     base64_init_decodestate(&s);
     cnt += base64_decode_block(base64_str, strlen(base64_str), (char *)dec_buf, &s);
     dec_size = cnt;
-    sg_vlbuf_insert(buf, dec_buf, dec_size);
+    sg_vsbuf_insert(buf, dec_buf, dec_size);
 
     return buf;
 }
 #endif
 
-int sg_base64_enc(const void *bin_buf, size_t bin_buf_len, sg_vlstr_t *b64_str)
+int sg_base64_enc(const void *bin_buf, size_t bin_buf_len, sg_vsstr_t *b64_str)
 {
     size_t b64_size;
     char *b64_buf;
@@ -114,13 +109,13 @@ int sg_base64_enc(const void *bin_buf, size_t bin_buf_len, sg_vlstr_t *b64_str)
     b64_size = _base64_calc_enc_len(bin_buf_len) + 1 /* Terminator */;
     b64_buf = b64_encode((const unsigned char *)bin_buf, bin_buf_len);
 
-    sg_vlstrcpy(b64_str, b64_buf);
+    sg_vsstr_cpy(b64_str, b64_buf);
     free(b64_buf);
     return 0;
 }
 
-int sg_base64_dec(const char *b64_str, size_t b64_str_len, sg_vlbuf_t *bin_buf)
-//struct sg_vlbuf *sg_base64_dec(const char *b64_str, size_t b64_str_len)
+int sg_base64_dec(const char *b64_str, size_t b64_str_len, sg_vsbuf_t *bin_buf)
+//struct sg_vsbuf *sg_base64_dec(const char *b64_str, size_t b64_str_len)
 {
     unsigned char *dec_buf;
     size_t dec_size;
@@ -134,7 +129,7 @@ int sg_base64_dec(const char *b64_str, size_t b64_str_len, sg_vlbuf_t *bin_buf)
         return 0;
 
     dec_buf = b64_decode_ex(b64_str, b64_str_len, &dec_size);
-    sg_vlbuf_insert(bin_buf, (const char*)(dec_buf), dec_size);
+    sg_vsbuf_insert(bin_buf, (const char*)(dec_buf), dec_size);
     free(dec_buf);
     return 0;
 }
