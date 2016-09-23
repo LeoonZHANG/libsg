11557aa0019cca5e2804f1b250596a4eb95062aa
metalwood <377307289@qq.com>
2016-09-23 13:10:49 +0800

    refactor speed module

--- src/math/speed.c
+++ src/math/speed.c
@@ -1,19 +1,20 @@
-#include <sg/math/speed.h>
-#include <sg/sys/time.h>
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
 #include <math.h>
-#include <assert.h>
+#include <sg/sg.h>
+#include <sg/math/speed.h>
+#include <sg/sys/clock.h>
+
 
 /* Define time grain as 20ms, this grain should not be too small since time precision and thread swith. */
-#define STATISTIC_GRAIN     20
+#define STATISTIC_GRAIN 20
 
 struct sg_speed_counter_real {
     int stat_duration_ms;
-    size_t* byte_slots;
-    size_t* head;
-    size_t* tail;
+    size_t *byte_slots;
+    size_t *head;
+    size_t *tail;
     unsigned long while_start;
     unsigned long grains_count;
 };
@@ -21,7 +22,7 @@ struct sg_speed_counter_real {
 
 struct sg_speed_val *sg_speed_alloc(void)
 {
-    struct sg_speed_val* result = calloc(1, sizeof(struct sg_speed_val));
+    struct sg_speed_val *result = calloc(1, sizeof(struct sg_speed_val));
     return result;
 }
 
@@ -49,7 +50,7 @@ static int calculate_2_exponent(enum sg_speed_unit old_unit, enum sg_speed_unit 
     return exponent;
 }
 
-int sg_speed_conv(struct sg_speed_val * speed, enum sg_speed_unit new_unit)
+int sg_speed_conv(struct sg_speed_val *speed, enum sg_speed_unit new_unit)
 {
     int exponent = calculate_2_exponent(speed->unit, new_unit);
     speed->val *= pow(2, exponent);
@@ -66,9 +67,9 @@ int sg_speed_conv_auto(struct sg_speed_val *speed)
     return 0;
 }
 
-int sg_speed_fmt_str(struct sg_speed_val * speed, const char *per_sec_str, char *dst_buf, size_t dst_buf_len)
+int sg_speed_fmt_str(struct sg_speed_val *speed, const char *per_sec_str, char *dst_buf, size_t dst_buf_len)
 {
-    const char* unit_strings[] = { "", "K", "M", "G", "T", "P", "Unkown" };
+    const char *unit_strings[] = { "", "K", "M", "G", "T", "P", "Unknown" };
     const int is_unit_in_byte_mode = speed->unit & 128;
 
     return snprintf(dst_buf, dst_buf_len, "%0.4f %s%s",
@@ -87,13 +88,13 @@ sg_speed_counter_t *sg_speed_counter_open(int stat_duration_ms)
     if (stat_duration_ms < 100)
         return NULL;
 
-    sg_speed_counter_t* result = malloc(sizeof(sg_speed_counter_t));
+    sg_speed_counter_t *result = malloc(sizeof(sg_speed_counter_t));
     result->stat_duration_ms = stat_duration_ms;
     result->byte_slots = calloc(counter_slots_capacities(result), sizeof(size_t));
-    // assign head as NULL idicates empty
+    /* assign head as NULL idicates empty */
     result->head = NULL;
     result->tail = result->byte_slots;
-    result->while_start = sg_boot_time_ms();
+    result->while_start = sg_clock_boot_time_ms();
     result->grains_count = 0;
     return result;
 }
@@ -109,7 +110,7 @@ void sg_speed_counter_close(sg_speed_counter_t * counter)
     }
 }
 
-static size_t counter_slots_size(sg_speed_counter_t* counter)
+static size_t counter_slots_size(sg_speed_counter_t *counter)
 {
     if (counter->head == NULL)
         return 0;
@@ -120,7 +121,7 @@ static size_t counter_slots_size(sg_speed_counter_t* counter)
         return counter_slots_capacities(counter) + 1 + distance;
 }
 
-static int counter_slots_is_full(sg_speed_counter_t* counter)
+static int counter_slots_is_full(sg_speed_counter_t *counter)
 {
     return counter_slots_size(counter) == counter_slots_capacities(counter);
 }
@@ -132,9 +133,9 @@ static void counter_slots_advance(sg_speed_counter_t* counter, size_t** pointer)
         (*pointer) = counter->byte_slots;
 }
 static int cccc = 0;
-static void counter_forward(sg_speed_counter_t* counter)
+static void counter_forward(sg_speed_counter_t *counter)
 {
-    // from empty to one, reset head
+    /* from empty to one, reset head */
     if (counter->head == NULL)
         counter->head = counter->tail;
 
@@ -147,9 +148,9 @@ static void counter_forward(sg_speed_counter_t* counter)
     }
 }
 
-static void try_fast_forward(sg_speed_counter_t * counter)
+static void try_fast_forward(sg_speed_counter_t *counter)
 {
-    const unsigned long now = sg_boot_time_ms();
+    const unsigned long now = sg_clock_boot_time_ms();
     unsigned long new_grains_count = (now - counter->while_start) / STATISTIC_GRAIN;
     unsigned long elapsed_grains = new_grains_count - counter->grains_count;
     
@@ -160,7 +161,7 @@ static void try_fast_forward(sg_speed_counter_t * counter)
     counter->grains_count = new_grains_count;
 }
 
-void sg_speed_counter_reg(sg_speed_counter_t * counter, size_t byte_size)
+void sg_speed_counter_reg(sg_speed_counter_t *counter, size_t byte_size)
 {
     try_fast_forward(counter);
     *counter->tail += byte_size;
@@ -173,11 +174,9 @@ int sg_speed_counter_read(sg_speed_counter_t *counter, struct sg_speed_val *spee
 
     try_fast_forward(counter);
 
-    if (slots_count > 0) {
-        for (size_t* p = counter->head; p != counter->tail; counter_slots_advance(counter, &p)) {
+    if (slots_count > 0)
+        for (size_t *p = counter->head; p != counter->tail; counter_slots_advance(counter, &p))
             total += *p;
-        }
-    }
     /* count up the incompleted slot as well */
     total += *counter->tail;
 
@@ -185,4 +184,3 @@ int sg_speed_counter_read(sg_speed_counter_t *counter, struct sg_speed_val *spee
     sg_speed_conv_auto(speed);
     return 0;
 }
-
