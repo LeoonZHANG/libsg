12254f6c4a38cf05e4ae107fe5f2ce605b1655fb
metalwood <377307289@qq.com>
2016-09-23 13:17:35 +0800

    refactor err

--- include/sg/util/err.h
+++ include/sg/util/err.h
@@ -13,61 +13,20 @@
 extern "C" {
 #endif /* __cplusplus */
 
-/* common errno in libsg */
-#define SG_ESTART 20000
-#define SG_EOUTOFMEMORY (SG_ESTART + 1)
-#define SG_EINVALIDARG  (SG_ESTART + 2)
+typedef enum sg_err {
+    SG_ERR_MIN = 0,
+    SG_OK = 0,
+    SG_ERR,
+    SG_ERR_MALLOC_FAIL,
+    SG_ERR_NULL_PTR,
+    SG_ERR_MAX
+} sg_err_t;
 
+/* you can add custom errno and errmsg in your project */
+void sg_err_add_custom(int err_no, const char *err_msg);
 
-void sg_err_add(int custom_errno, const char *custom_errmsg);
-
-/* sg_set_errno */
-void sg_err_errno_set(int errno);
-
-void sg_err_errno_set_and_log(int errno);
-
-void sg_err_errno_clear(void);
-
-int sg_err_errno_crt(void);
-
-#if defined(OS_WIN)
-
-int sg_err_errno_win(void);
-
-int sg_err_errno_wsa(void);
-
-#endif
-
-const char *sg_err_errmsg_crt(void);
-
-const char *sg_err_errmsg_win(void);
-
-const char *sg_err_errmsg_wsa(void);
-
-const char *sg_err_strerror(int errno);
-
-
-
-void sg_set_last_err(int errno);
-
-void sg_clear_last_err(void);
-
-int sg_get_last_err(void);
-
-const char *sg_get_last_err_msg(void);
-
-const char *sg_err_to_msg(int err_num);
-
-
-
-
-
-
-const char *sg_err_msg_std(void);
-
-const char *sg_err_msg_win(void);
-
-const char *sg_err_msg_wsa(void);
+/* transform err number to error message */
+const char *sg_err_fmt(sg_err_t err_no);
 
 #ifdef __cplusplus
 }
