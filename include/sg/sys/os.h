d7bfa3f61261eed4feda41d9b49dffdaa4b2f22a
metalwood <377307289@qq.com>
2016-09-24 18:14:38 +0800

    remove invalid macros

--- include/sg/sys/os.h
+++ include/sg/sys/os.h
@@ -13,28 +13,6 @@
 extern "C" {
 #endif /* __cplusplus */
 
-/* come from sdkddkver.h of Win7 SDK */
-/*
-#define _WIN32_WINNT_NT4                  0x0400
-#define _WIN32_WINNT_WIN2K                0x0500
-#define _WIN32_WINNT_WINXP                0x0501
-#define _WIN32_WINNT_WS03                 0x0502
-#define _WIN32_WINNT_WIN6                 0x0600
-#define _WIN32_WINNT_VISTA                0x0600
-#define _WIN32_WINNT_WS08                 0x0600
-#define _WIN32_WINNT_LONGHORN             0x0600
-#define _WIN32_WINNT_WIN7                 0x0601
-
-enum os_win_ver {
-    OSWINVER_2000  = 0,
-    OSWINVER_XP    = 1,
-    OSWINVER_VISTA = 2,
-    OSWINVER_7     = 3,
-    OSWINVER_8     = 4,
-    OSWINVER_10    = 5
-};
-*/
-
 #if defined(__MACH__) || defined(macintosh) || defined(__APPLE__) || defined(__APPLE_CC__)
 # define SG_OS_MACOS
 #elif defined(linux) || defined(__linux) || defined(__linux__)
