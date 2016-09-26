0569ff781ab31975c51e8aaabbb03960a91a4f5f
metalwood <377307289@qq.com>
2016-09-26 21:26:52 +0800

    rename module

--- include/sg/net/p2p_agent.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by metalwood on 16/9/25.
-//
-
-#ifndef LIBSG_P2P_AGENT_H
-#define LIBSG_P2P_AGENT_H
-
-#endif //LIBSG_P2P_AGENT_H
---
+++ include/sg/net/p2p_middle.h
@@ -0,0 +1,8 @@
+//
+// Created by metalwood on 16/9/25.
+//
+
+#ifndef LIBSG_P2P_MIDDLE_H
+#define LIBSG_P2P_MIDDLE_H
+
+#endif /* LIBSG_P2P_MIDDLE_H */
