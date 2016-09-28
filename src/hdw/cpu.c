e7783ec058f2272bc70dfb53a88e23315e640f48
metalwood <377307289@qq.com>
2016-09-28 14:51:36 +0800

    fix cpu number get method under macOS

--- src/hdw/cpu.c
+++ src/hdw/cpu.c
@@ -5,10 +5,13 @@
 
 #include <sg/sg.h>
 #include <sg/hdw/cpu.h>
+
 #if defined(SG_OS_WINDOWS)
 # include <windows.h>
-#else
+#elif defined(SG_OS_LINUX)
 # include <sys/sysinfo.h> /* get_nprocs */
+#elif defined(SG_OS_MACOS)
+# include <sys/sysctl.h> /* sysctl */
 #endif
 
 int sg_cpu_usable_count(void)
@@ -17,7 +20,27 @@ int sg_cpu_usable_count(void)
     SYSTEM_INFO info;
     GetSystemInfo(&info);
     return info.dwNumberOfProcessors;
-#else
+#elif defined(SG_OS_LINUX)
     return get_nprocs();
+#elif defined(SG_OS_MACOS)
+    int mib[4];
+    int cpu_num;
+    std::size_t len = sizeof(cpu_num); 
+    
+    /* set the mib for hw.ncpu */
+    mib[0] = CTL_HW;
+    mib[1] = HW_AVAILCPU;  /* alternatively, try HW_NCPU */
+    
+    /* get the number of CPUs from the system */
+    sysctl(mib, 2, &cpu_num, &len, NULL, 0);
+    
+    if (cpu_num < 1) {
+        mib[1] = HW_NCPU;
+        sysctl(mib, 2, &cpu_num, &len, NULL, 0);
+        if (cpu_num < 1)
+            cpu_num = 1;
+    }
+
+    return cpu_num;
 #endif
 }
